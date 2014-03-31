33a7a557109d586fb9b89f9e0162b40d72b00b6a
dvlanzisera <dvlanzisera@gmail.com>
2014-03-30 23:04:09 -0400

    Update Account.java
    Daily accrued interest is incorporated.  Some code will go after modifying other classes.

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -2,7 +2,7 @@ package com.abc;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.ReentrancLock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Account {
 
@@ -14,6 +14,8 @@ public class Account {
 
     private final int accountType;
     private int accountId;
+    private AccountBalance accountBalance;
+    private double totalInterest;
     public List<Transaction> transactions;
     /* Add reentrant lock for thread safety */
     private final ReentrantLock lock = new ReentrantLock();
@@ -22,8 +24,38 @@ public class Account {
         setAccountId();
         this.accountType = accountType;
         this.transactions = new ArrayList<Transaction>();
+        this.accountBalance = new AccountBalance();
+        this.totalInterest=0;
     }
+    
+    /* Add a simple inner class encapsulating accountBalance and the date of this balance */
+    class AccountBalance {
+        Date date;
+        double balance;
+        
+        AccountBalance () {
+            setBalance(0.0);
+        }
+        
+        setBalance(double balance) {
+            balance = balance;
+            date = DateProvider.getInstance().now();
+        }
+    }
+    
+    /* Current balance is the most recent balance plus interest earned */ 
+    private double currentBalance() {
+        Date currentdate = DateProvider.getInstance().now();
+        Date pastDate = accountBalance.date;
+        int daysBetween =  (int)( (currentDate.getTime() - pastDate.getTime()) / (1000 * 60 * 60 * 24));
+        double pastBalance = accountBalance.balance;
+        double newInterest = interestEarned(pastBalance,daysBetween);
+        this.totalInterest += newInterest;
+        return pastBalance + newInterest;
+     }
 
+    }
+    
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
@@ -31,25 +63,50 @@ public class Account {
             lock.lock();
             try {
                 transactions.add(new Transaction(amount));
+                accountBalance.setBalance(currentBalance()+amount);
             } finally {
                 lock.unlock();
             }
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        lock.lock();
-        try {
-            transactions.add(new Transaction(-amount));
-        } finally {
-            lock.unlock();
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        } else {
+            lock.lock();
+            try {
+                balance=currentBalance();
+                if (amount > balance) {
+                    throw new IllegalArgumentException("insufficient funds");
+                } else {
+                    transactions.add(new Transaction(-amount));
+                    accountBalance.setBalance(balance-amount);
+                }
+            } finally {
+                lock.unlock();
+            }
         }
     }
-}
-
+    /* Replacement function showing interest accruing daily */
+    public double interestEarned(double amount, int days) {
+        switch(accountType){
+            case SAVINGS:
+                if (amount <= 1000)
+                    return amount * 0.001 * (double) days / 365;
+                else
+                    return (1 + (amount-1000) * 0.002) * (double) days / 365;
+            case MAXI_SAVINGS:
+                if (amount <= 1000)
+                    return (amount * 0.02) * (double) days / 365;
+                if (amount <= 2000)
+                    return (20 + (amount-1000) * 0.05) * (double) days / 365;
+                return (70 + (amount-2000) * 0.1) * (double) days / 365;
+            default:
+                return (amount * 0.001) * (double) days / 365;
+        }
+    }
+    
     public double interestEarned() {
         double amount = sumTransactions();
         switch(accountType){
@@ -71,7 +128,6 @@ public void withdraw(double amount) {
                 return amount * 0.001;
         }
     }
-
     public double sumTransactions() {
        return checkIfTransactionsExist(true);
     }
@@ -100,4 +156,7 @@ public void withdraw(double amount) {
         destination.deposit(amount);
     }
 
+    public double getTotalInterest () {
+        return totalInterest;
+    }
 }
