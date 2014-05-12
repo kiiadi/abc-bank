7fd905ffc7ec3aa533a2a6ca9095997d54ca596f
bridgea <adam.bridge@formicary.net>
2014-05-12 11:42:27 -0400

    removed unused field transactionDate and unused import

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,15 +1,9 @@
 package com.abc;
 
-import java.util.Date;
-
 public class Transaction {
     public final double amount;
 
-    private Date transactionDate;
-
     public Transaction(double amount) {
         this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
     }
-
 }
