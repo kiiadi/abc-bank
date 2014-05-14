5157f3c21b6523f24264501120b3ad928886ddd1
jffrystn <jffrystn222@gmail.com>
2014-05-14 10:08:09 -0400

    Document Bank class
    1. Added Javadoc to the bank class
    2. Removed Bank::getFirstCustomer as it is not used anywhere else in
    the application.

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -3,17 +3,35 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * The Bank class
+ * 
+ * @author Jeff
+ *
+ */
 public class Bank {
     private List<Customer> customers;
 
+    /**
+     * Create a new bank object
+     */
     public Bank() {
         customers = new ArrayList<Customer>();
     }
 
+    /**
+     * Add a new customer to the bank
+     * @param customer the customer to add
+     */
     public void addCustomer(Customer customer) {
         customers.add(customer);
     }
 
+    /**
+     * Run the customer summary report
+     * @return a string representing the customer summary report.
+     */
     public String customerSummary() {
         String summary = "Customer Summary";
         for (Customer c : customers)
@@ -27,6 +45,10 @@ public class Bank {
         return number + " " + (number == 1 ? word : word + "s");
     }
 
+    /**
+     * calculate the total interest paid
+     * @return the interest paid.
+     */
     public double totalInterestPaid() {
         double total = 0;
         for(Customer c: customers)
@@ -34,13 +56,4 @@ public class Bank {
         return total;
     }
 
-    public String getFirstCustomer() {
-        try {
-            customers = null;
-            return customers.get(0).getName();
-        } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
-        }
-    }
 }
