5f658504fd68b08e8dd266e4fcc048bd9121597a
jffrystn <jffrystn222@gmail.com>
2014-05-14 22:12:28 -0400

    Created new TestSuite class
    TestSuite class created to run all regression tests in a batch.

---
+++ src/test/java/com/abc/TestSuite.java
@@ -0,0 +1,23 @@
+/**
+ * The Bank Application test suite
+ */
+package com.abc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   BankTest.class,
+   CustomerTest.class,
+   TransactionTest.class
+})
+
+/**
+ * The test suite class
+ * @author Jeff
+ *
+ */
+public class TestSuite
+	{
+
+	}
