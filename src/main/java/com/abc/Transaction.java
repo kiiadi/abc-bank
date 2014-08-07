0da5a6c5eae612ea84baa090f8a8b70f3c95ae59
sergkrs <sergkrs@gmail.com>
2014-08-07 17:31:23 -0400

    - Refactored code to encapsulate object state; - Added javadoc descriptions for all public API;

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,51 @@
 package com.abc;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
+/**
+ * The class keeps information about transaction.<br>
+ * After transaction creating it is not allowed to change its state.
+ */
 public class Transaction {
-    public final double amount;
+    private final double amount;
+    private final Date transactionDate;
 
-    private Date transactionDate;
-
+    /**
+     * Create Transaction instance for given amount with current transaction date.
+     */
     public Transaction(double amount) {
         this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
+        transactionDate = Calendar.getInstance().getTime();
+    }
+    
+    /**
+     * Create Transaction instance for given amount and transaction date.
+     */
+    public Transaction(double amount, Date date) {
+    	this.amount = amount;
+    	transactionDate = date;
+    }
+    
+    /**
+     * Return transaction amount.
+     */
+    public double getAmount() {
+    	return amount;
+    }
+    
+    /**
+     * Return transaction date.
+     */
+    public Date getDate() {
+    	return transactionDate;
+    }
+    
+    @Override
+    public String toString() {
+    	return String.valueOf(amount) + " " + new SimpleDateFormat("dd/MM/yyyy",Locale.getDefault()).format(transactionDate);
     }
 
 }
--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,27 @@
 package com.abc;
 
+import java.util.Calendar;
+import java.util.Date;
 import org.junit.Test;
-
 import static org.junit.Assert.assertTrue;
 
-public class TransactionTest {
-    @Test
-    public void transaction() {
+/**
+ * Unit test for {@link Transaction}
+ */
+public class TransactionTest {	
+    
+    /**
+	 * Test transaction creating.
+	 */
+	@Test
+    public void transactionCreation() {
         Transaction t = new Transaction(5);
         assertTrue(t instanceof Transaction);
+        assertTrue(t.getAmount() == 5 );
+        
+        Date d = Calendar.getInstance().getTime();
+        t = new Transaction(50.0, d);
+        assertTrue(t.getAmount() == 50.0 );
+        assertTrue(d.equals(t.getDate()));
     }
 }
