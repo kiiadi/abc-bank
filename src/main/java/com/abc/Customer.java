fc79a67228a7c77a14b03c3b47d321ca60a6080a
sergkrs <sergkrs@gmail.com>
2014-08-07 22:14:05 -0400

    - Refactored; - Moved transactional dependenciy to Account class for better decpoupling; - Added support for interest calculation on daily-based rates; - Added javadoc descriptions for all public API; - Mofified unit tests;

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -1,78 +1,111 @@
 package com.abc;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static java.lang.Math.abs;
 
+/**
+ * The class implements behavior of a bank customer.<br>
+ * The customer can have number of various bank accounts, 
+ * manipulate with them and provide total balance statement.
+ */
 public class Customer {
-    private String name;
+    private final String name;
     private List<Account> accounts;
 
+    /**
+     * Create Customer instance with given name.
+     */
     public Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
 
+    /**
+     * Return customer name.
+     */
     public String getName() {
         return name;
     }
 
-    public Customer openAccount(Account account) {
+    /**
+     * Add a new account.
+     */
+    public void openAccount(Account account) {
         accounts.add(account);
-        return this;
+    }
+    
+    /**
+     * Transfer amount between two accounts.
+     */
+    public void transfer(double amount, Account fromAccount, Account toAccount) {
+    	if (amount <= 0)
+    		throw new IllegalArgumentException("Amount must be greater than zero");
+    	
+    	double fromBallance = fromAccount.sumTransactions();
+    	if (fromBallance < amount)
+    		throw new IllegalArgumentException("Account balance is not allowed to overdraw");
+    	
+    	fromAccount.withdraw(amount);
+    	toAccount.deposit(amount);    	
     }
 
+    /**
+     * Return number of accounts for the customer.
+     */
     public int getNumberOfAccounts() {
         return accounts.size();
     }
 
+    /**
+     * Calculate total interest earned by customer until now.
+     */
     public double totalInterestEarned() {
         double total = 0;
         for (Account a : accounts)
             total += a.interestEarned();
         return total;
     }
-
-    public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
-        }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
+    
+    /**
+     * Calculate total interest earned by customer given date.
+     */
+    public double totalInterestEarned(Date date) {
+        double total = 0;
+        for (Account a : accounts)
+            total += a.interestEarned(date);
+        return total;
     }
 
-    private String statementForAccount(Account a) {
-        String s = "";
-
-       //Translate to pretty account type
-        switch(a.getAccountType()){
-            case Account.CHECKING:
-                s += "Checking Account\n";
-                break;
-            case Account.SAVINGS:
-                s += "Savings Account\n";
-                break;
-            case Account.MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
-                break;
-        }
-
-        //Now total up all the transactions
+    /**
+     * Return pretty representation of customer info.
+     */
+    public String getStatement() {
+    	StringBuilder sb = new StringBuilder("Statement for ");
+    	sb.append(name);
+    	sb.append("\n\n");
+    	
         double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
+        for (Account a : accounts) {
+        	sb.append(a.getStatement());
+        	sb.append("\n");
+            total += a.sumTransactions();
         }
-        s += "Total " + toDollars(total);
-        return s;
+        sb.append("Total In All Accounts ");
+        sb.append(toDollars(total));
+        sb.append("\n");
+        return sb.toString();
     }
 
     private String toDollars(double d){
         return String.format("$%,.2f", abs(d));
     }
+    
+    @Override
+    public String toString() {
+    	return String.format("%s (%d accopunts(s))\n", name, accounts.size() );
+    }
+    
 }
--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -3,55 +3,189 @@ package com.abc;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+
+/**
+ * Unit test for {@link Customer}
+ */
 public class CustomerTest {
+	private static final double DOUBLE_DELTA = 1e-15;
 
-    @Test //Test customer statement generation
-    public void testApp(){
-
-        Account checkingAccount = new Account(Account.CHECKING);
-        Account savingsAccount = new Account(Account.SAVINGS);
-
-        Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
-
-        checkingAccount.deposit(100.0);
-        savingsAccount.deposit(4000.0);
-        savingsAccount.withdraw(200.0);
-
-        assertEquals("Statement for Henry\n" +
-                "\n" +
-                "Checking Account\n" +
-                "  deposit $100.00\n" +
-                "Total $100.00\n" +
-                "\n" +
-                "Savings Account\n" +
-                "  deposit $4,000.00\n" +
-                "  withdrawal $200.00\n" +
-                "Total $3,800.00\n" +
-                "\n" +
-                "Total In All Accounts $3,900.00", henry.getStatement());
-    }
-
+	/**
+	 * Test customer statement generation.	
+	 */
     @Test
-    public void testOneAccount(){
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
-        assertEquals(1, oscar.getNumberOfAccounts());
-    }
+    public void statement() {
+        Account checkingAccount = new Account(AccountType.CHECKING);
+        Account savingsAccount = new Account(AccountType.SAVINGS);
 
+        Customer henry = new Customer("Henry");
+        henry.openAccount(checkingAccount);
+        henry.openAccount(savingsAccount);
+
+        Date d1 = Account.getDateFromString("January 1, 2014");
+		Date d2 = Account.getDateFromString("February 1, 2014");
+		Date d3 = Account.getDateFromString("March 1, 2014");
+        checkingAccount.deposit(100.0, d1);
+        savingsAccount.deposit(4000.0, d2);
+        savingsAccount.withdraw(200.0, d3);
+        
+        StringBuilder sb = new StringBuilder("Statement for Henry\n\n");
+        sb.append("Checking Account (1 transaction)\n");
+        sb.append( "  01/01/2014 deposit $100.00\n");
+        sb.append("Total $100.00\n\n");
+        sb.append("Saving Account (2 transactions)\n");
+        sb.append("  02/01/2014 deposit $4,000.00\n");
+        sb.append("  03/01/2014 withdrawal $200.00\n");
+        sb.append("Total $3,800.00\n\n");
+        sb.append("Total In All Accounts $3,900.00\n");
+        
+        assertTrue(sb.toString().equals(henry.getStatement()));
+    }
+    
+    /**
+	 * Test total interest earned  by customer.	
+	 * Earned interests are calculated on daily basis.
+	 */
     @Test
-    public void testTwoAccount(){
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
-        assertEquals(2, oscar.getNumberOfAccounts());
+    public void totalInterestEarned(){
+        Account checkingAccount = new Account(AccountType.CHECKING);
+        Account savingsAccount = new Account(AccountType.SAVINGS);
+        
+        // Checking account has a flat rate of 0.1%.
+     	double rate = InterestRates.CHECKING_RATE.getVal() / 365;
+     	// Saving account has a rate of 0.1% for the first $1,000 then 0.2%.
+     	double rateMax = InterestRates.SAVING_RATE.getVal() / 365;
+
+        Customer henry = new Customer("Henry");
+        henry.openAccount(checkingAccount);
+        henry.openAccount(savingsAccount);
+        
+        /* 
+		 * Test calculation for 1 month.
+		 * We added $1000 to checking account on January, 1.
+		 * We expect amount * 31 days * daily rate.
+		 */
+        Date d1 = Account.getDateFromString("January 1, 2014");
+        Date d2 = Account.getDateFromString("February 1, 2014");
+        checkingAccount.deposit(1000.0, d1);
+        double expect = 1000.0 * 31 * rate;
+		double earned = henry.totalInterestEarned(d2);		
+		assertTrue(Math.abs(earned - expect) <= DOUBLE_DELTA);
+		
+		/* 
+		 * Test calculation for 2 months.
+		 * We added $4000 to saving account on February, 1.
+		 * Earning is calculated with increased rate for balance above $1,000.
+		 * We expect result as sum of the previous January and new February:
+		 * - for Feb (checking account): 1000 * 28 days * daily rate;		
+		 * - for Feb (saving account first 1000): 1000 * 28 days * daily rate;
+		 * - for Feb (saving account above 1000): 3000 * 31 days * daily rate max;
+		 * There can be some small dispersion in calculation results.
+		 */        
+		Date d3 = Account.getDateFromString("March 1, 2014");
+        savingsAccount.deposit(4000.0, d2);
+        expect += 1000.0 * 28 * rate;
+        expect += 1000.0 * 28 * rate;
+        expect += 3000.0 * 28 * rateMax;
+        earned = henry.totalInterestEarned(d3);	
+		assertTrue(Math.abs(earned - expect) <= DOUBLE_DELTA);
+		
+		/* 
+		 * Test calculation for 3 months.
+		 * We withdraw $2000 from saving account on March, 1.
+		 * Earning is calculated with increased rate for balance above $1,000.
+		 * We expect result as sum of the previous January and February plus new March:
+		 * - for Mar (checking account): 100 * 31 days * daily rate;
+		 * - for Mar (saving account first 1000): 1000 * 31 days * daily rate;
+		 * - for Mar (saving account above 1000): 1000 * 31 days * daily rate max;
+		 * There can be some small dispersion in calculation results.
+		 */ 
+		Date d4 = Account.getDateFromString("April 1, 2014");        
+        savingsAccount.withdraw(2000.0, d3);
+        expect += 1000.0 * 31 * rate;
+        expect += 1000.0 * 31 * rate;
+        expect += 1000.0 * 31 * rateMax;
+        earned = henry.totalInterestEarned(d4);	
+        assertTrue(Math.abs(earned - expect) <= DOUBLE_DELTA);
+    }
+    
+    /**
+     * Test transfer balances between two accounts.
+     */
+    @Test
+    public void transfer() {    	
+    	Account checkingAccount = new Account(AccountType.CHECKING);
+        Account savingsAccount = new Account(AccountType.SAVINGS);
+        Customer oscar = new Customer("Oscar");
+        oscar.openAccount(checkingAccount);
+        oscar.openAccount(savingsAccount);
+        
+        // Deposit $1000 to checking account and then transfer $500 to saving account.
+        // We expect both account balances to be equal.
+        checkingAccount.deposit(1000.0);
+        oscar.transfer(500.0, checkingAccount, savingsAccount);
+        assertTrue(checkingAccount.sumTransactions() == savingsAccount.sumTransactions());
+        
+    }
+    
+    /**
+     * Test transfer balances between two accounts with wrong amount.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void badTransfer() {    	
+    	Account checkingAccount = new Account(AccountType.CHECKING);
+        Account savingsAccount = new Account(AccountType.SAVINGS);
+        Customer oscar = new Customer("Oscar");
+        oscar.openAccount(checkingAccount);
+        oscar.openAccount(savingsAccount);
+        
+        // Deposit $1000 to checking account and then transfer $500 to saving account.
+        // Try to transfer negative amount or amount that excess balance of checking account.
+        // We expect error about overdraft protection be raised.
+        checkingAccount.deposit(1000.0);
+        oscar.transfer(-500.0, checkingAccount, savingsAccount); 
+        oscar.transfer(500.0, checkingAccount, savingsAccount); 
+        oscar.transfer(1500.0, checkingAccount, savingsAccount);
+    }
+    
+    private void checkCustomer(Customer customer, String name, int number) {
+    	assertTrue(name.equals(customer.getName()));
+    	assertTrue(number == customer.getNumberOfAccounts());
     }
 
-    @Ignore
-    public void testThreeAcounts() {
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
-        assertEquals(3, oscar.getNumberOfAccounts());
+    /**
+	 * Test customer creating and opening one account.
+	 */
+    @Test
+    public void oneAccount(){
+        Customer oscar = new Customer("Oscar");
+        oscar.openAccount(new Account(AccountType.SAVINGS));
+        checkCustomer(oscar, "Oscar", 1);
+    }
+
+    /**
+	 * Test customer creating and opening two accounts.
+	 */
+    @Test
+    public void twoAccount(){
+        Customer oscar = new Customer("Oscar");
+        oscar.openAccount(new Account(AccountType.SAVINGS));
+        oscar.openAccount(new Account(AccountType.CHECKING));
+        checkCustomer(oscar, "Oscar", 2);
+    }
+
+    /**
+	 * Test customer creating and opening three accounts.
+	 */
+    @Ignore @Test 
+    public void threeAcounts() {
+        Customer oscar = new Customer("Oscar");
+        oscar.openAccount(new Account(AccountType.SAVINGS));
+        oscar.openAccount(new Account(AccountType.CHECKING));
+        oscar.openAccount(new Account(AccountType.MAXI_SAVINGS));
+        checkCustomer(oscar, "Oscar", 3);
     }
 }
