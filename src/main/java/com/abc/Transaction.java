7842c67edd58be54aadd1e388aa04b6d2acbddf8
ashokathikam <ashok.athikam@gmail.com>
2014-08-25 00:43:29 -0400

    Added getTransactionDate method
    Changed the DateProvider instantiation logic to guarantee single instance.

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,6 +1,6 @@
 package com.abc;
 
-import java.util.Calendar;
+// Removed the unused import Calendar
 import java.util.Date;
 
 public class Transaction {
@@ -10,7 +10,12 @@ public class Transaction {
 
     public Transaction(double amount) {
         this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
+        this.transactionDate = DateProvider.now();
+    }
+    
+    //Return Defensive copy, so that date is not modified by the caller.
+    public Date getTransactionDate(){
+      return new Date(this.transactionDate.getTime());
     }
 
 }
