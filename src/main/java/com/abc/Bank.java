6406de851315e5094a7419a67a60f5737e5c62a9
sanjuthomas <sanju@sanju.org>
2014-09-28 21:58:18 -0400

    fixing typo

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -27,16 +27,10 @@ public class Bank {
     	return null;
     }
 
-    //Make sure correct plural of word is created based on the number passed in:
-    //If number passed in is 1 just return the word otherwise add an 's' at the end
-    private String format(int number, String word) {
-        return number + " " + (number == 1 ? word : word + "s");
-    }
-
     public double totalInterestPaid() {
         double total = 0;
         for(ICustomer c: customers)
-            total += c.totalInterestEarned();
+            total += c.getTotalInterestEarned();
         return total;
     }
 
