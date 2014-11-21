672010fff9bebbab62f0437ab29d8ab32b2c1e6a
xaqseid <zekariasseid@gmail.com>
2014-11-21 01:14:21 -0800

    Update Transaction.java

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -2,6 +2,7 @@ package com.abc;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Transaction {
     public final double amount;
@@ -12,5 +13,25 @@ public class Transaction {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
     }
+    
+    public Transaction(double amount, int days_before){
+    	this.amount = amount;
+    	long days = (long) (10*((1000*60*60*24)));
+    	//Long days = TimeUnit.MILLISECONDS.convert(days_before, TimeUnit.DAYS);
+    	Calendar calendar = Calendar.getInstance();
+    	calendar.setTimeInMillis(calendar.getTimeInMillis() - days);
+    	Date previous_date = calendar.getTime();
+    	this.transactionDate = previous_date;
+    }
+    
+    public Transaction(double amount, Date date){
+    	this.amount = amount;
+    	this.transactionDate = date;
+    }
+    
+    public Date getTransactionDate(){
+    	return transactionDate;
+    }
 
 }
+
