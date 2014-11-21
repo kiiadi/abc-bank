dfa764e751e57c05b3a1917091844ce4628aa94f
hcfremontca <hcfremontca@gmail.com>
2014-11-21 14:11:16 -0800

    Update Bank.java

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -40,16 +40,6 @@ public class Bank {
     }
    
 
-    // building String should use StringBuilder. Commented out by Henry Chen
-   /* public String customerSummary() {
-        String summary = "Customer Summary";
-        for (Customer c : customers)
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
-        return summary;
-    }*/
-    
-    
-
     //Make sure correct plural of word is created based on the number passed in:
     //If number passed in is 1 just return the word otherwise add an 's' at the end
     private String format(int number, String word) {
@@ -69,18 +59,4 @@ public class Bank {
         
     }
 
-    // This method has tow bugs and commented out by Henry Chen
-    // 1. customer is always null
-    // 2. catch clause will not cacth null point exception 
-   /* public String getFirstCustomer() {
-    	
-    	
-        try {
-            customers = null;
-            return customers.get(0).getName();
-        } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
-        }
-    }*/
 }
