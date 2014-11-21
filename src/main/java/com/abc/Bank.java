a634d8ea63d2d263aa39a33c878be700609c5d1b
hcfremontca <hcfremontca@gmail.com>
2014-11-21 14:01:22 -0800

    Update Bank.java

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -1,25 +1,54 @@
 package com.abc;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Bank {
-    private List<Customer> customers;
+	
+	private final static double EPSLON = 1e-15;
+
+	private Map<String, Customer> customers;
 
     public Bank() {
-        customers = new ArrayList<Customer>();
+        customers = new LinkedHashMap<String, Customer>();
     }
 
     public void addCustomer(Customer customer) {
-        customers.add(customer);
+    	//Modified by Henry chen
+    	if(customer == null) return;
+        customers.put(customer.getName(), customer);
     }
-
+    
     public String customerSummary() {
+    	StringBuilder summary = new StringBuilder("Customer Summary");
+    	for (Customer c : customers.values())
+            summary.append("\n - ").append(c.getName()).append(" (").append(format(c.getNumberOfAccounts(), "account")).append(")");
+        return summary.toString();
+    }
+    
+    public void transfer (String name, double ammount, String fromAccountNumber,  String toAccountNumber) {
+    	if(!customers.containsKey(name))
+    		throw new IllegalArgumentException(name + " does not have an account with us.");
+    	
+    	Customer customer = customers.get(name);
+    	Account fromAccount = customer.getAccount(fromAccountNumber);
+    	Account toAccount = customer.getAccount(toAccountNumber);
+    	
+    	fromAccount.withdraw(ammount);
+        toAccount.deposit(ammount);
+		
+    }
+   
+
+    // building String should use StringBuilder. Commented out by Henry Chen
+   /* public String customerSummary() {
         String summary = "Customer Summary";
         for (Customer c : customers)
             summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
         return summary;
-    }
+    }*/
+    
+    
 
     //Make sure correct plural of word is created based on the number passed in:
     //If number passed in is 1 just return the word otherwise add an 's' at the end
@@ -29,12 +58,23 @@ public class Bank {
 
     public double totalInterestPaid() {
         double total = 0;
-        for(Customer c: customers)
+        for(Customer c: customers.values())
             total += c.totalInterestEarned();
         return total;
     }
-
+    
     public String getFirstCustomer() {
+    	
+       return (customers.isEmpty())?null:customers.values().iterator().next().getName();
+        
+    }
+
+    // This method has tow bugs and commented out by Henry Chen
+    // 1. customer is always null
+    // 2. catch clause will not cacth null point exception 
+   /* public String getFirstCustomer() {
+    	
+    	
         try {
             customers = null;
             return customers.get(0).getName();
@@ -42,5 +82,5 @@ public class Bank {
             e.printStackTrace();
             return "Error";
         }
-    }
+    }*/
 }
