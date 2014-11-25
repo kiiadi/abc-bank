9a1d04d89deeb4ba55d1f9b5c08ba0e220998e7e
vinoth <vinothkumarc@gmail.com>
2014-11-24 21:14:27 -0500

    Commit After all changes

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -1,46 +1,58 @@
-package com.abc;
+package com.abc; 
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
 
 public class Bank {
-    private List<Customer> customers;
+	
+    private List<Customer> customers; 
+    
+    //I assume, I dont need to (over) think much about the (ArrayList) data structure being used.
+    //List of Customers for Bank, List of Accounts for Customers etc.
+    
+    public Bank() { 
+        customers = new ArrayList<Customer>(); 
+    } 
+    
+    public void addCustomer(Customer customer) { 
+        customers.add(customer); 
+    } 
+    
+    public String customerSummary() { 
+        String summary = "Customer Summary"; 
+        for (Customer c : customers) 
+            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")"; 
+        return summary; 
+    } 
+    
+    //Make sure correct plural of word is created based on the number passed in: 
+    //If number passed in is 1 just return the word otherwise add an 's' at the end 
+    private String format(int number, String word) { 
+        return number + " " + (number == 1 ? word : word + "s"); 
+    } 
+    
+    public double totalInterestPaid() { 
+        double total = 0; 
+        for(Customer c: customers) 
+            total += c.totalInterestEarned(); 
+        return total; 
+    } 
 
-    public Bank() {
-        customers = new ArrayList<Customer>();
-    }
-
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
-
-    public String customerSummary() {
-        String summary = "Customer Summary";
-        for (Customer c : customers)
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
-        return summary;
-    }
-
-    //Make sure correct plural of word is created based on the number passed in:
-    //If number passed in is 1 just return the word otherwise add an 's' at the end
-    private String format(int number, String word) {
-        return number + " " + (number == 1 ? word : word + "s");
-    }
-
-    public double totalInterestPaid() {
-        double total = 0;
-        for(Customer c: customers)
-            total += c.totalInterestEarned();
-        return total;
-    }
-
-    public String getFirstCustomer() {
-        try {
-            customers = null;
-            return customers.get(0).getName();
-        } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
-        }
-    }
-}
+    public double totalDailyInterestAccrued() {
+        double totalDailyInterestAccrual = 0; 
+        for(Customer c: customers) 
+        	totalDailyInterestAccrual += c.totalDailyInterestAccrued(); 
+        return totalDailyInterestAccrual; 
+    } 
+    
+  	public String getFirstCustomer() 
+  	{ 
+        try { 
+            customers = null; 
+            return customers.get(0).getName(); 
+        } catch (Exception e){ 
+            e.printStackTrace(); 
+            return "Error"; 
+        } 
+    } 
+} 
--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -3,16 +3,34 @@ package com.abc;
 import java.util.Calendar;
 import java.util.Date;
 
+//I assume that the intention of this singleton class is to have a single point access
+// to retrieve all date related services. Ex. if required, instead of getting current date from the
+//Calendar (as doing below), you can get it from a configuration file or any other persistence. So,
+// I continued to use it further. Also if required, new method(s) can be added to return any specific date needs.
+
 public class DateProvider {
-    private static DateProvider instance = null;
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
-
-    public Date now() {
-        return Calendar.getInstance().getTime();
-    }
+	private static final int MAXI_SAVINGS_REBATE_WITHDRAWL_DAYS_LIMIT=10;
+	
+	private static DateProvider instance = null;
+	
+	private DateProvider() {}
+	
+	public static DateProvider getInstance() 
+	{ 
+       if (instance == null) 
+           instance = new DateProvider(); 
+       return instance; 
+	} 
+   
+	public Date now() 
+	{ 
+	   return Calendar.getInstance().getTime(); 
+	}
+	
+	public Date rebateEligibleDate()
+	{
+		Date transactionDate = now();
+		return  new Date (transactionDate.getTime() - MAXI_SAVINGS_REBATE_WITHDRAWL_DAYS_LIMIT);
+	}
 }
