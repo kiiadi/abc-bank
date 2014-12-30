2f894f5f9ade8d7c34373af24d2315a2ac295f69
mnkanojia <kanojia.manish@gmail.com>
2014-12-29 23:58:20 -0500

    Commiting the code with my changes

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -3,20 +3,41 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class represents the account for a customer. Accounts can be of 
+ * 3 types - CHECKING, SAVINGS OR MAXI_SAVINGS.
+ * Using instance of this class transactions such as deposit and withdraw 
+ * can be performed for a customer. Also, 
+ * @author Manish
+ *
+ */
 public class Account {
 
-    public static final int CHECKING = 0;
-    public static final int SAVINGS = 1;
-    public static final int MAXI_SAVINGS = 2;
+	enum AccountType {
+		CHECKING("Checking Account"), SAVINGS("Saving Account"), MAXI_SAVINGS("Maxi Savings Account");
+		String accountTypeDesc;
+		private AccountType(String accountTypeDesc) {
+			this.accountTypeDesc=accountTypeDesc;
+		}
+	};
 
-    private final int accountType;
+    private final AccountType accountType;
     public List<Transaction> transactions;
 
-    public Account(int accountType) {
+    /**
+     * Constructs the account object with account type as parameter
+     * @param accountType
+     */
+    public Account(AccountType accountType) {
         this.accountType = accountType;
         this.transactions = new ArrayList<Transaction>();
     }
 
+    /**
+     * Adds a deposit transaction to transactions list for this account,
+     * with an amount greater than zero.
+     * @param amount
+     */
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
@@ -25,25 +46,33 @@ public class Account {
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
+    /**
+     * Adds a withdraw transaction to transactions list for this account,
+     * with an amount greater than zero.
+     * @param amount
+     */
+	public void withdraw(double amount) {
+	    if (amount <= 0) {
+	        throw new IllegalArgumentException("amount must be greater than zero");
+	    } else {
+	        transactions.add(new Transaction(-amount));
+	    }
+	}
 
+	/**
+	 * For this account this method returns the 
+	 * @return
+	 */
     public double interestEarned() {
         double amount = sumTransactions();
+        if(amount==0.0d) {
+        	return 0.0d;
+        }
         switch(accountType){
             case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
+            	return (amount <= 1000) ? 
+            			(amount * 0.001) : 
+            			(1 + (amount-1000) * 0.002);
             case MAXI_SAVINGS:
                 if (amount <= 1000)
                     return amount * 0.02;
@@ -55,18 +84,25 @@ public void withdraw(double amount) {
         }
     }
 
+    /**
+     * This method returns sum of all the transactions for this account.
+     * @return
+     */
     public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
-
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
+    	if(transactions.size()==0) {
+    		return 0.0d;
+    	}
+        double amount = 0.0d;
         for (Transaction t: transactions)
             amount += t.amount;
         return amount;
     }
 
-    public int getAccountType() {
+    /**
+     * Returns the account type of this account.
+     * @return
+     */
+    public AccountType getAccountType() {
         return accountType;
     }
 
--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -3,11 +3,17 @@ package com.abc;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class TransactionTest {
-    @Test
+    private static final double DOUBLE_DELTA = 1e-15;
+
+	@Test
     public void transaction() {
         Transaction t = new Transaction(5);
         assertTrue(t instanceof Transaction);
+        assertEquals(5, t.getAmount(), DOUBLE_DELTA);
+        assertNotNull("Date cannot be null", t.getTransactionDate());
     }
 }
