260552cab527695885b797d3939514596bde5380
darrenmc <darren@kurralta.com>
2015-05-31 14:53:37 +0100

    Combine total interest paid tests into one

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -3,11 +3,11 @@ package com.abc;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class BankTest {
-    private static final double DOUBLE_DELTA = 1.0e-15;
-
 
     private Bank bank;
     private Customer john;
@@ -51,36 +51,21 @@ public class BankTest {
     }
 
     @Test
-    public void checkingAccount() {
+    public void totalInterestPaid() {
         Account checkingAccount = new Account(Account.CHECKING);
         john.openAccount(checkingAccount);
-        bank.addCustomer(john);
-
         checkingAccount.deposit(100.0);
 
-        assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
-
-    @Test
-    public void savingsAccount() {
         Account savingsAccount = new Account(Account.SAVINGS);
         john.openAccount(savingsAccount);
-        bank.addCustomer(john);
-
         savingsAccount.deposit(1500.0);
 
-        assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
-
-    @Test
-    public void maxiSavingsAccount() {
         Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
         john.openAccount(maxiSavingsAccount);
-        bank.addCustomer(john);
-
         maxiSavingsAccount.deposit(3000.0);
 
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
+        bank.addCustomer(john);
 
+        assertThat(bank.totalInterestPaid(), equalTo(172.1));
+    }
 }
