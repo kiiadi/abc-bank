b460de015d67a60b31c4634d44d0ba9c05faacac
jjhansi1999 <jjhansi1999@yahoo.com>
2015-11-05 22:38:44 -0500

    Update DateProvider.java

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -3,15 +3,26 @@ package com.abc;
 import java.util.Calendar;
 import java.util.Date;
 
+/*
+ * Change this to SingleTon as we need to keep only 1 instance
+ */
 public class DateProvider {
     private static DateProvider instance = null;
-
+    
+    private DateProvider()
+    {
+    }
     public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
+    	
+        if (instance != null) {
+        	return instance;
+        }
+        synchronized (DateProvider.class) {
+        	if(instance == null)
+                instance = new DateProvider();
+		}
         return instance;
     }
-
     public Date now() {
         return Calendar.getInstance().getTime();
     }
