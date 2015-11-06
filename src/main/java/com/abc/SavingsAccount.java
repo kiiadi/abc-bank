4308e63bd903ae95b18a58d546044039bb7da6f9
jjhansi1999 <jjhansi1999@yahoo.com>
2015-11-05 22:40:05 -0500

    Create SavingsAccount.java

---
+++ src/main/java/com/abc/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.abc;
+
+public class SavingsAccount extends Account {
+	public SavingsAccount(int accountType) {
+		super(accountType);
+	}
+
+	@Override
+	public double interestEarned() {
+		double amount = sumTransactions();
+		if (amount <= 1000) {
+			return amount * 0.001;
+		} else {
+			return (1000*0.001)+(amount - 1000) * 0.002;
+		}
+	}
+
+}
