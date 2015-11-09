7992dbd687c0820c233913091ba511db79801578
PankajMankotia <pankaj.mankotia87@gmail.com>
2015-11-09 01:08:41 -0500

    Update Bank.java

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -1,9 +1,11 @@
 package com.abc;
-
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
+	private static final String ACCOUNT = "account";
+	private static final String CUSTOMER_SUMMARY = "Customer Summary";
     private List<Customer> customers;
 
     public Bank() {
@@ -15,10 +17,10 @@ public class Bank {
     }
 
     public String customerSummary() {
-        String summary = "Customer Summary";
+        StringBuilder summary = new StringBuilder(CUSTOMER_SUMMARY);
         for (Customer c : customers)
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
-        return summary;
+            summary.append("\n - ").append(c.getName()).append(" (").append(format(c.getNumberOfAccounts(), ACCOUNT)).append(")"); 
+        return return summary.toString();
     }
 
     //Make sure correct plural of word is created based on the number passed in:
@@ -27,10 +29,10 @@ public class Bank {
         return number + " " + (number == 1 ? word : word + "s");
     }
 
-    public double totalInterestPaid() {
-        double total = 0;
+    public BigDecimal totalInterestPaid() {
+        BigDecimal total = BigDecimal.ZERO;
         for(Customer c: customers)
-            total += c.totalInterestEarned();
+            total = total.add(c.totalInterestEarned());
         return total;
     }
 
