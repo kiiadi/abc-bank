5554295b8c0e46be0b075c4f841c68fe2bf53f36
Anandsahu92 <anand92sahu@gmail.com>
2016-04-18 13:04:26 -0400

    Added files via upload

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -2,12 +2,32 @@ package com.abc;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class TransactionTest {
     @Test
     public void transaction() {
         Transaction t = new Transaction(5);
         assertTrue(t instanceof Transaction);
+    }
+    
+    //test to check getTranscation and getAmount are working 
+    @Test
+    public void testGetterMethods(){
+    	 Account checkingAccount = new Account(Account.CHECKING);
+    	 Customer henry = new Customer("Henry").openAccount(checkingAccount);
+    	 checkingAccount.deposit(100.0);
+    	 checkingAccount.withdraw(10);
+    	 List<Transaction> t= henry.getAccounts().get(henry.getAccounts().size()-1).transactions;
+    	
+    	 Transaction first = t.get(0);
+    	 assertEquals(100, first.getAmount(),0.0);
+    	 Calendar cal = Calendar.getInstance();
+    	 cal.setTime(first.getTranscationDate());
+    	 assertEquals(2, cal.get(Calendar.DAY_OF_WEEK));
+    	 
     }
 }
